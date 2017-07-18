a24c423a80b3b28d7491f4755e2f149998d298a5
Student <student@lab-315-010.cs.csubak.edu>
2017-07-18 11:05:00 -0700

    text changes

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,10 +17,16 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
+                TextView tv = (TextView)findViewById(R.id.textView1);
+                tv.setText("Rodrigo");c
                 Toast.makeText(getApplicationContext(), "Rodrigo", Toast.LENGTH_LONG).show();
             }
         });
     }
 }
+
+
+
 
